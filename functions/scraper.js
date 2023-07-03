d7cd75a57f69a2f20c614254da4f8f494c3a3829
chavalk <chavalk@hotmail.com>
2023-07-03 10:21:56 -0500

    Add table reference.

--- functions/scraper.js
+++ functions/scraper.js
@@ -7,17 +7,18 @@ const scrapeData = async () => {
 
     const page = await browser.newPage();
 
-    await page.goto('https://en.wikipedia.org/wiki/Main_Page', {
-        waitUntil: "domcontentloaded"
+    await page.goto('https://live.house.gov/', {
+        waitUntil: "networkidle0"
     });
 
     const body = await page.evaluate(() => {
-        const imgReference = document.querySelector('#mp-otd #mp-otd-img img');
-        const listReference = document.querySelectorAll('#mp-otd > ul li');
+        // const imgReference = document.querySelector('#mp-otd #mp-otd-img img');
+        // const listReference = document.querySelectorAll('#mp-otd > ul li');
+        const tableReference = document.querySelectorAll('#activity-table > tbody tr');
 
-        let imgSource = imgReference.getAttribute('src');
+        // let imgSource = imgReference.getAttribute('src');
 
-        return imgSource;
+        return tableReference;
     });
 
     browser.close();
