9e4f29a97f869e38fc7f59dc27078f489afcf881
chavalk <chavalk@hotmail.com>
2023-07-04 21:23:53 -0500

    Configure index.js to export function to firebase.

--- functions/index.js
+++ functions/index.js
@@ -9,6 +9,32 @@
 
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
+const functions = require("firebase-functions");
+const scraper = require("./scraper");
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+const db = admin.firestore();
+
+const getToday = () => {
+    const today = new Date();
+
+    return `${today.getDate()}${today.getMonth() + 1}${today.getFullYear()}`;
+};
+
+exports.pubsub = functions
+    .region("us-central1")
+    .runWith({ memory: '2GB' })
+    .pubsub.schedule("0 0 * * *")
+    .timeZone("America/Chicago")
+    .onRun(async () => {
+        try {
+            const scrapeData = await scraper.scrapeData();
+            await db.collection('activity').doc(getToday()).set(scrapeData);
+        } catch (error) {
+            throw new Error(error);
+        }
+    })
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
