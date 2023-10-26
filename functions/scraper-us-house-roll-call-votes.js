0a7e01c03397fd1a3507066c20eba874a89006e9
chavalk <chavalk@hotmail.com>
2023-10-26 10:17:53 -0500

    Update scraper to avoid function crashing when no votes exist.

--- functions/scraper-us-house-roll-call-votes.js
+++ functions/scraper-us-house-roll-call-votes.js
@@ -51,67 +51,75 @@ const scrapeUSHouseRollCallVotes = async () => {
             return rollCallLinks;
         });
 
-        // Navigate to roll call page of first index in rollCallActivity array
-        await page.goto(body[0].link, {
-            waitUntil: 'networkidle0'
-        });
+        // Create empty votes array in order for it to be accessible to be returned when votes exist
+        let votes = []
 
-        // Get votes for roll call of first index in rollCallActivity array
-        const votes = await page.evaluate(() => {
-            // Scrape bill from roll call page
-            const bill = document.querySelector('body').childNodes[6].textContent
-            // Scrape time from roll call page
-            const time = document.querySelector('body').childNodes[7].textContent
-            // Scrape roll call question from roll call page
-            const question = document.querySelector('body').childNodes[11].textContent
-            // Remove spaces from question scraped from roll call page
-            const formattedQuestion = question.replace(/\s+/, '')
-            // Scrape bill title from roll call page
-            const billTitle = document.querySelector('body').childNodes[15].textContent
-            // Get all tables in roll call page
-            const tablesReference = document.querySelectorAll('table');
-
-            // Convert tables reference to array and iterate through them to extract data cells
-            const tables = Array.from(tablesReference).map((item) => {
-                // Gett all data cells from table
-                const itemData = item.querySelectorAll('tbody tr td');
-
-                // Put data cells in an array
-                const itemDataArray = Array.from(itemData);
-
-                // Concatenate columns into one object and return it. If third row doesn't exist, return empty string
-                return {
-                    item: itemDataArray[0].innerText + itemDataArray[1].innerText + itemDataArray[2]?.innerText || ''
-                }
+        // Check if there are any votes existing. If there are no votes, an empty object is returned. If there are votes, the votes are scraped from the page.
+        if (body.length === 0) {
+            return {}
+        } else {
+            // Navigate to roll call page of first index in rollCallActivity array
+            await page.goto(body[0].link, {
+                waitUntil: 'networkidle0'
             });
 
-            // Remove first index from tables array since it references total votes table
-            tables.splice(0, 1);
+            // Get votes for roll call of first index in rollCallActivity array
+            votes = await page.evaluate(() => {
+                // Scrape bill from roll call page
+                const bill = document.querySelector('body').childNodes[6].textContent
+                // Scrape time from roll call page
+                const time = document.querySelector('body').childNodes[7].textContent
+                // Scrape roll call question from roll call page
+                const question = document.querySelector('body').childNodes[11].textContent
+                // Remove spaces from question scraped from roll call page
+                const formattedQuestion = question.replace(/\s+/, '')
+                // Scrape bill title from roll call page
+                const billTitle = document.querySelector('body').childNodes[15].textContent
+                // Get all tables in roll call page
+                const tablesReference = document.querySelectorAll('table');
 
-            // Reformat first table to have house members last name separated by a comma
-            tables[0].item = tables[0].item.replaceAll('\n', ', ');
-            const yesVotes = tables[0].item.split(', ')
+                // Convert tables reference to array and iterate through them to extract data cells
+                const tables = Array.from(tablesReference).map((item) => {
+                    // Gett all data cells from table
+                    const itemData = item.querySelectorAll('tbody tr td');
 
-            // Reformat second table to have house members last name separated by a comma
-            tables[1].item = tables[1].item.replaceAll('\n', ', ');
-            const noVotes = tables[1].item.split(', ')
+                    // Put data cells in an array
+                    const itemDataArray = Array.from(itemData);
 
-            // Reformat third table to have house members last name separated by a comma
-            tables[2].item = tables[2].item.replaceAll('\n', ', ');
-            const notVoting = tables[2].item.split(', ')
+                    // Concatenate columns into one object and return it. If third row doesn't exist, return empty string
+                    return {
+                        item: itemDataArray[0].innerText + itemDataArray[1].innerText + itemDataArray[2]?.innerText || ''
+                    }
+                });
 
-            // Return correctly formatted tables
-            return {
-                id: '1',
-                bill: bill,
-                time: time,
-                question: formattedQuestion,
-                billTitle: billTitle,
-                yesVotes: yesVotes, 
-                noVotes: noVotes,
-                notVoting: notVoting
-            }
-        });
+                // Remove first index from tables array since it references total votes table
+                tables.splice(0, 1);
+
+                // Reformat first table to have house members last name separated by a comma
+                tables[0].item = tables[0].item.replaceAll('\n', ', ');
+                const yesVotes = tables[0].item.split(', ')
+
+                // Reformat second table to have house members last name separated by a comma
+                tables[1].item = tables[1].item.replaceAll('\n', ', ');
+                const noVotes = tables[1].item.split(', ')
+
+                // Reformat third table to have house members last name separated by a comma
+                tables[2].item = tables[2].item.replaceAll('\n', ', ');
+                const notVoting = tables[2].item.split(', ')
+
+                // Return correctly formatted tables
+                return {
+                    id: '1',
+                    bill: bill,
+                    time: time,
+                    question: formattedQuestion,
+                    billTitle: billTitle,
+                    yesVotes: yesVotes,
+                    noVotes: noVotes,
+                    notVoting: notVoting
+                }
+            });
+        }
 
         // Close Puppeteer browser
         await browser.close();
@@ -127,9 +135,9 @@ const scrapeUSHouseRollCallVotes = async () => {
 
 // Call scrapeUSHouseVotes function
 scrapeUSHouseRollCallVotes()
-.then((res) => {
-    console.log(res);
-});
+    .then((res) => {
+        console.log(res);
+    });
 
 // Export scrapeUSHouseVotes function
 exports.scrapeUSHouseRollCallVotes = scrapeUSHouseRollCallVotes;
