56cb519f52b57ae6f024cf468579ec1e0807b6e8
chavalk <chavalk@hotmail.com>
2023-11-27 22:39:08 -0600

    Comment out scrape us house roll call votes.

--- functions/index.js
+++ functions/index.js
@@ -58,42 +58,42 @@ exports.scrapeUSHouseFloorActivity = functions
     })
 
 // Build Firebase Cloud function to scrape roll call votes in the US House of Representatives
-exports.scrapeUSHouseRollCallVotes = functions
-    // Define region where function will be deployed
-    .region("us-central1")
-    // Allocate how much memory the function needs
-    .runWith({ memory: '2GB' })
-    // Schedule function using pubsub feature and chrome expression in unix-cron to run every day at midnight
-    .pubsub.schedule("0 0 * * *")
-    // Set time zone for function to follow
-    .timeZone("America/Chicago")
-    // Set function to run asynchronously
-    .onRun(async () => {
-        try {
-            // Call scrapeData function from scraper.js
-            const scrapedUSHouseRollCallVotes = await scraperUSHouseRollCallVotes.scrapeUSHouseRollCallVotes();
-            // for (let i = 0; i < scrapedUSHouseRollCallVotes.length; i++) {
-            //     // Extract roll call number from object in array in order to use it as document id in database
-            //     var rollCallNumber = scrapedUSHouseRollCallVotes[i].rollCallNumber;
-            //     // Convert roll call number to string
-            //     var rollCallNumberString = rollCallNumber.toString();
-            //     // Make call to Firebase to create collection called activity, to create document using current date as the name of the document, and set data in document from scrapeData
-            //     db.collection('votes').doc(rollCallNumberString).set(scrapedUSHouseRollCallVotes[i]);
-            // }
-            // Check if scrapedUSHouseRollCallVotes is returning an empty object
-            if (JSON.stringify(scrapedUSHouseRollCallVotes) == '{}') {
-                // Return if object is empty
-                return
-            } else {
-                // Make call to database to save votes if object is not empty
-                return db.collection('votes').doc(scrapedUSHouseRollCallVotes.id).set(scrapedUSHouseRollCallVotes);
-            }
-        } catch (error) {
-            // Console log error in case execution fails
-            console.log('Error ocurred during function execution:', error);
-            return null;
-        }
-    })
+// exports.scrapeUSHouseRollCallVotes = functions
+//     // Define region where function will be deployed
+//     .region("us-central1")
+//     // Allocate how much memory the function needs
+//     .runWith({ memory: '2GB' })
+//     // Schedule function using pubsub feature and chrome expression in unix-cron to run every day at midnight
+//     .pubsub.schedule("0 0 * * *")
+//     // Set time zone for function to follow
+//     .timeZone("America/Chicago")
+//     // Set function to run asynchronously
+//     .onRun(async () => {
+//         try {
+//             // Call scrapeData function from scraper.js
+//             const scrapedUSHouseRollCallVotes = await scraperUSHouseRollCallVotes.scrapeUSHouseRollCallVotes();
+//             // for (let i = 0; i < scrapedUSHouseRollCallVotes.length; i++) {
+//             //     // Extract roll call number from object in array in order to use it as document id in database
+//             //     var rollCallNumber = scrapedUSHouseRollCallVotes[i].rollCallNumber;
+//             //     // Convert roll call number to string
+//             //     var rollCallNumberString = rollCallNumber.toString();
+//             //     // Make call to Firebase to create collection called activity, to create document using current date as the name of the document, and set data in document from scrapeData
+//             //     db.collection('votes').doc(rollCallNumberString).set(scrapedUSHouseRollCallVotes[i]);
+//             // }
+//             // Check if scrapedUSHouseRollCallVotes is returning an empty object
+//             if (JSON.stringify(scrapedUSHouseRollCallVotes) == '{}') {
+//                 // Return if object is empty
+//                 return
+//             } else {
+//                 // Make call to database to save votes if object is not empty
+//                 return db.collection('votes').doc(scrapedUSHouseRollCallVotes.id).set(scrapedUSHouseRollCallVotes);
+//             }
+//         } catch (error) {
+//             // Console log error in case execution fails
+//             console.log('Error ocurred during function execution:', error);
+//             return null;
+//         }
+//     })
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
