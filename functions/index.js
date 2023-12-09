0c7812f78b6218a2d4a06e4b4d271cff43145d35
chavalk <chavalk@hotmail.com>
2023-12-08 20:20:56 -0600

    Remove commented out code.

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,7 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-//const {onRequest} = require("firebase-functions/v2/https");
-//const logger = require("firebase-functions/logger");
 // Import the Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers
 const functions = require("firebase-functions");
 // Import scraper.js file from current functions folder
 const scraperUSHouseFloorActivity = require("./scraper-us-house-floor-activity");
-// Import scrape-us-house-votes.js from current functions folder
-// const scraperUSHouseRollCallVotes = require("./scraper-us-house-roll-call-votes")
 // Import the Firebase Admin SDK to access Firestore
 const admin = require("firebase-admin");
 
