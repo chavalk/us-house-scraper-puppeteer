c8cc109a66a2a1c9ff2e0ab44e99125d929b2139
chavalk <chavalk@hotmail.com>
2024-06-06 16:35:45 -0500

    Reformat bill number for better readability by users.

--- functions/scraper-us-house-roll-call-votes.js
+++ functions/scraper-us-house-roll-call-votes.js
@@ -56,7 +56,10 @@ const scrapeUSHouseRollCallVotes = async () => {
                         const rollCallNumberString = rollCallNumberTitleParts[6];
                         const rollCallNumber = parseInt(rollCallNumberString, 10);
 
+                        // Scrape roll call bill number and reformat it for better readability by users
                         const billNumber = document.querySelector('body').childNodes[6].textContent;
+                        const parts = billNumber.split(' ');
+                        const reformattedBillNumber = `${parts[0]}.${parts[1]}. ${parts.slice(2).join(' ')}`;
 
                         // Scrape roll call date and time and reformat it to comply to Firestore timestamp format
                         const dataAndTime = document.querySelector('body').childNodes[7].textContent;
@@ -138,7 +141,7 @@ const scrapeUSHouseRollCallVotes = async () => {
 
                         return {
                             id: timestamp,
-                            billNumber: billNumber,
+                            billNumber: reformattedBillNumber,
                             timestamp: timestamp,
                             rollCallNumber: rollCallNumber,
                             question: formattedQuestion,
