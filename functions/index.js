5fc4ffc1223a0a1fcb90060e34b13d34af9ee18d
chavalk <chavalk@hotmail.com>
2024-09-21 18:46:22 -0500

    Import firebase-functions/v1 in order to fix a breaking change from a new package version.

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-const functions = require("firebase-functions");
+const functions = require("firebase-functions/v1");
 const scraperUSHouseRollCallVotes = require("./scraper-us-house-roll-call-votes");
 const admin = require("firebase-admin");
 const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
